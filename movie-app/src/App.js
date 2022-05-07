656f3a93424dc1977d004f3b926b5a7295b9049a
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-05-07 18:02:05 +0200

    Added Header and fetching data from oMDB API

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,10 +1,31 @@
+import { useEffect } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const API_URL = 'http://www.omdbapi.com?apikey=45057cc6';
 
-    </div>
+function App() {
+
+  const searchMovies = async (title) => {
+
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('Batman');
+  }, []);
+
+
+  return (
+    <header className='header'>
+      <div className='container center'>
+        <h1 className='logo'>MovieBox7</h1>
+      </div>
+    </header>
+
   );
 }
 
