1f4575205758a219b65ab8e6735336df38ecaab3
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-05-08 12:03:57 +0200

    fetch data

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,45 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const API_URL = 'http://api.weatherapi.com/v1/current.json?key=cb4e6bb47f94480083b83356220805&q=';
 
 function App() {
+  const [place, setPlace] = useState('');
+  const [locationInfo, setLocationInfo] = useState({});
+
+  useEffect(() => {
+    handleFetch();
+  }, []);
+
+  const handleFetch = () => {
+    fetch(`${API_URL}${place}&aqi=no`)
+      .then(response => response.json())
+      .then(data => setLocationInfo({
+        name: data.location.name,
+        country: data.location.country,
+        tempCelsius: data.current.temp_c,
+        condition: data.current.condition.text,
+      }))
+  };
+
+
   return (
     <div className="App">
-      <h1>Weather app</h1>
+
+
+      <section >
+        <div >
+          <input type="text" value={place} onChange={(e) => setPlace(e.target.value)} />
+          <button className='btn' onClick={handleFetch}>Search</button>
+        </div>
+
+        <div className="weather-info">
+          <h2>{locationInfo.name} {locationInfo.country}</h2>
+          <p>{locationInfo.tempCelsius}</p>
+          <p>{locationInfo.condition}</p>
+        </div>
+
+      </section>
     </div>
   );
 }
