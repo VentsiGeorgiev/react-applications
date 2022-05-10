1f9e8719db9de3e155ddd410981655c30753ec29
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-05-10 17:31:18 +0200

    Added feedback form component, selecting rating and adding a new rating

--- feedback/src/App.js
+++ feedback/src/App.js
@@ -1,5 +1,7 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from "react";
 import FeedbackStats from "./components/FeedbaackStats";
+import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
 
 import Header from "./components/Header";
@@ -15,10 +17,16 @@ function App() {
     }
   }
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback])
+  }
+
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
