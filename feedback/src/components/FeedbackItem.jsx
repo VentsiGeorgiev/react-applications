f512cab5d1703ff9e94258db8e791cafbf577190
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-05-10 15:02:14 +0200

    Delete feedback using prop drilling

--- feedback/src/components/FeedbackItem.jsx
+++ feedback/src/components/FeedbackItem.jsx
@@ -1,10 +1,23 @@
 import Card from './shared/Card';
+import { FaTimes } from 'react-icons/fa'
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
+
+
 
   return (
     <Card>
       <div className="num">{item.rating}</div>
+
+
+      <div className="buttons">
+        <button onClick={() => handleDelete(item.id)} className='close'>
+          <FaTimes />
+        </button>
+      </div>
+
+
+
       <div className="text">{item.text}</div>
     </Card>
   )
--- feedback/src/components/FeedbackList.jsx
+++ feedback/src/components/FeedbackList.jsx
@@ -1,17 +1,17 @@
 import FeedbackItem from "./FeedbackItem";
 
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
 
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback</p>
   }
 
   return (
-    <div>
+    <div className="container">
       {feedback.map((item) => (
         <>
-          <FeedbackItem key={item.id} item={item} />
+          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
         </>
       ))}
     </div>
