c454f7fcbdb073f3067e0eb22251c7afc5e51242
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-05-13 09:09:14 +0200

    Adding and styling navbar component

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
 
-    </div>
+      <Navbar />
+
+
+
+      <main>Content</main>
+    </Router>
   );
 }
 
---
+++ github-finder/src/components/layout/Navbar.jsx
@@ -0,0 +1,29 @@
+import { FaGithub } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+function Navbar() {
+  return (
+    <nav className='nav'>
+      <div className='container flex'>
+
+        <div className='logo'>
+          <FaGithub />
+          <Link className='logo-name' to='/'>
+            GitHub Finder
+          </Link>
+        </div>
+
+        <div className='links'>
+          <Link className='link' to='/'>Home</Link>
+          <Link className='link' to='/about'>About</Link>
+        </div>
+
+      </div>
+    </nav>
+  )
+}
+
+
+
+export default Navbar
