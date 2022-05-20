506909f31b5860b4fe8f303cdd18e4e16f025a96
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-05-20 12:55:11 +0200

    Added React toastify alerts

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
@@ -24,6 +26,8 @@ function App() {
         <Navbar />
 
       </Router>
+
+      <ToastContainer />
     </>
   )
 }
