776bb91b50fb0e602605f515ab7ba967b3450bbb
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-05-25 09:42:34 +0200

    Added explore page and fetch listings from firebase

--- house-marketplace/src/pages/Explore.jsx
+++ house-marketplace/src/pages/Explore.jsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router-dom"
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
+
 function Explore() {
   return (
     <div>
-      <h1>Explore</h1>
+      <header>
+        <p>Explore</p>
+      </header>
+
+      <main>
+        <p>Categories</p>
+        <div className="exploreCategory">
+          <Link to='/category/rent'>
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImage"
+            />
+            <p>Places for rent</p>
+          </Link>
+          <Link to='/category/sale'>
+            <img
+              src={sellCategoryImage}
+              alt="sell"
+              className="exploreCategoryImage"
+            />
+            <p>Places for sale</p>
+          </Link>
+        </div>
+      </main>
     </div>
   )
 }
