67c6682add84654ac6cb371370485f3a8eecee65
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-06-12 15:21:30 +0200

    Added button component

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,17 +1,42 @@
+import { useReducer } from 'react';
+import DigitButton from './components/DigitButton';
 import './style.css'
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, { type, payload }) {
+
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand}${payload.digit}`
+      }
+  }
+
+}
 
 
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
+
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">1000</div>
-        <div className="current-operand">*222</div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
 
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <button>/</button>
+      <DigitButton digit='/' dispatch={dispatch} />
       <button>1</button>
       <button>2</button>
       <button>3</button>
