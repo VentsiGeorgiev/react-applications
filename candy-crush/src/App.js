7a84c0fe41398f41bae4f386d0ff7de66b7e768c
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-24 12:00:36 +0200

    Check for matching colors on column of three and four;

--- candy-crush/src/App.js
+++ candy-crush/src/App.js
@@ -52,19 +52,58 @@ function App() {
         }
     }, [arrangeColors]);
 
+
+    // # Check for row of three
+    const checkFoRowOfThree = useCallback(() => {
+        for (let i = 0; i < 64; i++) {
+            const rowOfThree = [i, i + 1, i + 2];
+            const decidedColor = arrangeColors[i];
+
+            const invalidIndex = [6, 7, 13, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64];
+
+            if (invalidIndex.includes(i)) {
+                continue;
+            }
+
+            if (rowOfThree.every(square => arrangeColors[square] === decidedColor)) {
+                rowOfThree.forEach(square => arrangeColors[square] = '');
+            }
+        }
+    }, [arrangeColors]);
+
+    // # Check for row of four
+    const checkFoRowOfFour = useCallback(() => {
+        for (let i = 0; i < 64; i++) {
+            const rowOfFour = [i, i + 1, i + 2, i + 3];
+            const decidedColor = arrangeColors[i];
+
+            const invalidIndex = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64];
+
+            if (invalidIndex.includes(i)) {
+                continue;
+            }
+
+            if (rowOfFour.every(square => arrangeColors[square] === decidedColor)) {
+                rowOfFour.forEach(square => arrangeColors[square] = '');
+            }
+        }
+    }, [arrangeColors]);
+
     useEffect(() => {
 
         const timer = setTimeout(() => {
 
             checkForColumnOfThree();
             checkForColumnOfFour();
+            checkFoRowOfThree();
+            checkFoRowOfFour();
             setArrangeColors([...arrangeColors]);
         }, 100);
 
         return () => {
             clearTimeout(timer);
         };
-    }, [checkForColumnOfThree, arrangeColors, checkForColumnOfFour]);
+    }, [checkForColumnOfThree, arrangeColors, checkForColumnOfFour, checkFoRowOfThree, checkFoRowOfFour]);
 
 
 
