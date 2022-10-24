a45af4e0adec0dce0d7734ea39c2c30234549e6b
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-24 11:10:48 +0200

    Generate Colors

--- candy-crush/src/App.js
+++ candy-crush/src/App.js
@@ -1,7 +1,48 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const width = 8;
+const candyColors = [
+    'blue',
+    'green',
+    'orange',
+    'purple',
+    'red',
+    'yellow',
+];
+
+
 function App() {
 
+    const [arrangeColors, setArrangeColors] = useState([]);
+
+    const generateColors = () => {
+        const boardColors = [];
+        for (let i = 0; i < width * width; i++) {
+            boardColors.push(candyColors[Math.floor(Math.random() * candyColors.length)]);
+        }
+        setArrangeColors(boardColors);
+    };
+
+    useEffect(() => {
+        generateColors();
+    }, []);
+
+
     return (
-        <div></div>
+        <div className='app'>
+            <div className='app-wrapper'>
+                {arrangeColors.map((color, index) => (
+                    <div
+                        key={index}
+                        className='color'
+                        style={{ backgroundColor: color }}
+                    >
+
+                    </div>
+                ))}
+            </div>
+
+        </div>
     );
 }
 
